c064d7f9bd42123d7ab3239b8e0ea2e0102de114
Codex-2005 <shivam.j@technmanconsulting.com>
2025-02-19 14:30:38 -0500

    post

--- src/app/api/users/verify/route.ts
+++ src/app/api/users/verify/route.ts
@@ -1,7 +1,7 @@
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
         const reqBody = await req.json();
         const { token } = reqBody;
@@ -12,7 +12,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
             verifyToken : token,
             verifyTokenExpiry: { $gt: Date.now() }
         })
-        
+
         if(!user){
             return NextResponse.json({ error: "Invalid or expired token" }, { status: 401 });
         }
